2a9cf1edd868789a47e8c790c05e7e01c6e20cd4
GenaPlem <shainkill1337@gmail.com>
2024-10-31 14:38:51 +0000

    Add path revalidation to update cached data

--- actions/posts.js
+++ actions/posts.js
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(prevState, formData) {
   const title = formData.get("title");
@@ -50,5 +51,6 @@ export async function createPost(prevState, formData) {
 }
 
 export async function togglePostLikeStatus(postId) {
-  updatePostLikeStatus(postId, 2);
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
 }
